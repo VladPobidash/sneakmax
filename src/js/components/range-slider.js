2e1f96a1cc3f20857989e595a259ed2b89c89b70
Vlad Pobidash <vladpobidash@gmail.com>
2021-10-18 21:11:30 +0300

    catalog filters

---
+++ src/js/components/range-slider.js
@@ -0,0 +1,34 @@
+const rangeSlider = document.getElementById("range-slider");
+
+if (rangeSlider) {
+  noUiSlider.create(rangeSlider, {
+    start: [500, 999999],
+    connect: true,
+    step: 1,
+    range: {
+      min: [500],
+      max: [999999],
+    },
+  });
+
+  const input0 = document.getElementById("input-0");
+  const input1 = document.getElementById("input-1");
+  const inputs = [input0, input1];
+
+  rangeSlider.noUiSlider.on("update", (values, handle) => {
+    inputs[handle].value = Math.round(values[handle]);
+  });
+
+  const setRangeSlider = (i, value) => {
+    let arr = [null, null];
+    arr[i] = value;
+
+    rangeSlider.noUiSlider.set(arr);
+  };
+
+  inputs.forEach((el, idx) => {
+    el.addEventListener("change", (e) =>
+      setRangeSlider(idx, e.currentTarget.value)
+    );
+  });
+}
